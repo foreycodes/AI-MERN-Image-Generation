a77e51cb38bbe77fdf05cbbc9d3949717a667365
eforrester <esfoures@gmail.com>
2023-01-25 14:12:22 -0500

    Initialize dalle route

--- server/index.js
+++ server/index.js
@@ -2,17 +2,29 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js'; 
+import dalleRoutes from './routes/dalleRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb'}));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 app.get('/', (req, res) => {
     res.send('Hello from Dall-E');
 })
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server has started on port 8080'));
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server has started on port 8080'));
+    } catch (err) {
+        console.log(err);
+    }
 }
 startServer(); 
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai'; 
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send('Hello from dall-E!');
+});
+
+export default router;
