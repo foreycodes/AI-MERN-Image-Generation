c1dadc2ea3bd56c516ae1a13fa46a9c30ec3bf70
eforrester <esfoures@gmail.com>
2023-01-26 10:57:04 -0500

    Fix server load

--- server/index.js
+++ server/index.js
@@ -15,9 +15,11 @@ app.use(express.json({ limit: '50mb'}));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello from Dall-E');
-})
+app.get('/', async (req, res) => {
+    res.status(200).json({
+      message: 'Hello from DALL.E!',
+    });
+  });
 
 const startServer = async () => {
     try {
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
 const connectDB = (url) => {
-    mongoose.set('strictQuery', true); 
+  mongoose.set('strictQuery', true);
+  
+  mongoose.connect(url)
+    .then(() => console.log('connected to mongo'))
+    .catch((err) => {
+      console.error('failed to connect with mongo');
+      console.error(err);
+    });
+};
 
-    mongoose.connect(url)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
-}
-
-export default connectDB; 
+export default connectDB;
