4f86a64e51fcbba745b02a928c4907ec29b9c479
eforrester <esfoures@gmail.com>
2023-01-29 14:01:02 -0500

    Create styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
           <>
             {searchText && (
               <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                Showing Resuls for <span className="text-[#222328]">{searchText}</span>:
+                Showing Resuls for <span className="text-[#22232-]">{searchText}</span>:
               </h2>
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
